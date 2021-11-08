33dc40f35559c7eabfb0d242601caf883db7d642
Kevin Pavao <kpavao84@gmail.com>
2021-11-07 23:59:55 -0600

    add eyebrowse, upgrade packages

--- lisp/kpav-windows-and-buffers.el
+++ lisp/kpav-windows-and-buffers.el
@@ -26,5 +26,26 @@
   :config
   (windmove-default-keybindings))
 
+(use-package eyebrowse
+  :straight t
+  :general
+  (base-leader-def
+    :states 'normal
+    "w." 'eyebrowse-switch-to-window-config
+    "w," 'eyebrowse-rename-window-config
+    "w1" 'eyebrowse-switch-to-window-config-1
+    "w2" 'eyebrowse-switch-to-window-config-2
+    "w3" 'eyebrowse-switch-to-window-config-3
+    "w4" 'eyebrowse-switch-to-window-config-4
+    "w4" 'eyebrowse-switch-to-window-config-4
+    "w5" 'eyebrowse-switch-to-window-config-5
+    "w6" 'eyebrowse-switch-to-window-config-6
+    "w7" 'eyebrowse-switch-to-window-config-7
+    "w8" 'eyebrowse-switch-to-window-config-8
+    "w9" 'eyebrowse-switch-to-window-config-9
+    "w0" 'eyebrowse-switch-to-window-config-0)
+  :config
+  (eyebrowse-mode t))
+
 (provide 'kpav-windows-and-buffers)
 ;;; kpav-windows-and-buffers.el ends here
