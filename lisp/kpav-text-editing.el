4d33907ba965b90a8376cc7c195a0a52c2847860
Kevin Pavao <kpavao84@gmail.com>
2023-05-25 22:39:14 -0500

    add anzu for find/replace

---
+++ lisp/kpav-text-editing.el
@@ -0,0 +1,9 @@
+;;; kpav-text-editing.el --- -*- lexical-binding: t -*-
+
+(use-package anzu
+  :straight t
+  :config
+  (global-anzu-mode +1))
+
+(provide 'kpav-defaults)
+;;; kpav-text-editing.el ends here
